9274b1d5ea8b31d46b6c1174eb77f979343d7cfa
Quang Huy <20211435@eaut.edu.vn>
2024-12-26 16:52:15 +0700

    Send Email with Template

--- src/main/java/vn/hoidanit/jobhunter/controller/EmailController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/EmailController.java
@@ -6,20 +6,27 @@ import org.springframework.web.bind.annotation.RestController;
 
 import vn.hoidanit.jobhunter.Util.annotation.ApiMessage;
 import vn.hoidanit.jobhunter.service.EmailService;
+import vn.hoidanit.jobhunter.service.SubscriberService;
 
 @RestController
 @RequestMapping("/api/v1")
 public class EmailController {
     private final EmailService emailService;
+    private final SubscriberService subscriberService;
 
-    public EmailController(EmailService emailService) {
+    public EmailController(EmailService emailService,
+            SubscriberService subscriberService) {
         this.emailService = emailService;
+        this.subscriberService = subscriberService;
     }
 
     @GetMapping("/email")
     @ApiMessage("Send simple email")
     public String sendSimpleEmail() {
-        this.emailService.sendSimpleEmail();
+        // this.emailService.sendSimpleEmail();
+        // this.emailService.sendEmailFromTemplateSync("quanghuydao17@gmail.com", "Test
+        // Send Email", "job");
+        this.subscriberService.sendSubscribersEmailJobs();
         return "ok";
     }
 }
